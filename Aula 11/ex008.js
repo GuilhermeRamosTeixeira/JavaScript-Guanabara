52d7ce5c792a2d8b08e8a9f99084d7f7c1306124
GuilhermeRamosTeixeira <guilherme.98ramos@hotmail.com>
2022-04-13 22:40:10 -0300

    Exercícios das aulas 09,10,11 e 12

---
+++ Aula 11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 60.5
+console.log("a velocidade do seu carro é " + vel)
+if (vel > 60) { // condição simples . não possui a parte do else
+    console.log("vc passou a velocidade permitida! foi multado")
+}
+
+
+
+
---
+++ Aula 11/ex009.js
@@ -0,0 +1,7 @@
+var pais = "EUA"
+
+if(pais=='Brasil'){
+    console.log('vc é Brasileiro')
+}else{
+    console.log('vc é entrangeiro')
+}
